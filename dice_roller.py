63ab68fcdac6c76f0d4e151f243aa679b1d49abd
Paul Evans <pevans@pulsesecure.net>
2020-09-17 17:15:13 +0100

    multi dice, multi faced

--- dice_roller.py
+++ dice_roller.py
@@ -1,22 +1,22 @@
 import random
 def main():
 
-  dice_rolls = 3
+  dice_rolls = 0
   dice_sum = 0
 
+  dice_rolls = int(input('How many dice to roll? '))
+  dice_size = int(input('How many faces on the dice? '))
+
   for i in range(0, dice_rolls):
-
-    roll = random.randint(1, 6)
-
+    roll = random.randint(1, dice_size)
     if roll == 1:
       print(f'You rolled a {roll}! Critical fail!')
-    elif roll == 6:
+    elif roll == dice_size:
       print(f'Whoa, a {roll}')
     else:
       print(f'You rolled a {roll}')
-
     dice_sum = dice_sum + roll
-  print(f'Total: {dice_sum}')
+  print(f'Total sum: {dice_sum}')
 
 if __name__== "__main__":
   main()
