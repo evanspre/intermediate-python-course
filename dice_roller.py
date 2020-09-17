5131f60ba68ad0a139a87e9429de494e14b94413
Paul Evans <pevans@pulsesecure.net>
2020-09-17 16:55:05 +0100

    Adding multi dice and sum

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,22 @@
 import random
 def main():
-  roll = random.randint(1, 6)
-  print(f'You rolled a {roll}')
+
+  dice_rolls = 3
+  dice_sum = 0
+
+  for i in range(0, dice_rolls):
+
+    roll = random.randint(1, 6)
+
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical fail!')
+    elif roll == 6:
+      print(f'Whoa, a {roll}')
+    else:
+      print(f'You rolled a {roll}')
+
+    dice_sum = dice_sum + roll
+  print(f'Total: {dice_sum}')
 
 if __name__== "__main__":
   main()
